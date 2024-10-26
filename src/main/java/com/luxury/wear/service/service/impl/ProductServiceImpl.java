78bf9bf5efe3da0751a5c065943028d28c09d49e
DavidBlanco2825 <davidfernando2825@hotmail.com>
2024-10-26 11:58:48 -0500

    feat: Implement service layer for Product
    - Defined the service layer implementations in `ProductServiceImpl` for handling core business logic.

--- src/main/java/com/luxury/wear/service/service/impl/ProductServiceImpl.java
+++ src/main/java/com/luxury/wear/service/service/impl/ProductServiceImpl.java
@@ -2,39 +2,68 @@ package com.luxury.wear.service.service.impl;
 
 import com.luxury.wear.service.entity.Product;
 import com.luxury.wear.service.repository.ProductRepository;
-import com.luxury.wear.service.service.IService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.luxury.wear.service.service.ProductService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class ProductServiceImpl implements IService<Product> {
-    @Autowired
-    private ProductRepository productRepository;
+@AllArgsConstructor
+public class ProductServiceImpl implements ProductService {
+
+    private final ProductRepository productRepository;
+
+    private static final String PRODUCT_NOT_FOUND_ID = "Product not found with Id: ";
 
     @Override
-    public List<Product> findAll() {
+    public Product createProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    @Override
+    public Product GetProductByID(Long id) {
+        return productRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException(PRODUCT_NOT_FOUND_ID + id));
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
         return productRepository.findAll();
     }
 
     @Override
-    public Product findById(Long id) {
-        return null;
+    public List<Product> getAllTopProducts() {
+        return productRepository.findAllRandom();
     }
 
     @Override
-    public Product save(Product product) {
-        return null;
+    public Product updateProduct(Product product) {
+        Product existingProduct = productRepository.findById(product.getProductId())
+                .orElseThrow(() -> new RuntimeException(PRODUCT_NOT_FOUND_ID + product.getProductId()));
+
+        return updateExistingProduct(existingProduct, product);
     }
 
     @Override
-    public Product update(Product product) {
-        return null;
+    public void deleteProductById(Long id) {
+        productRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException(PRODUCT_NOT_FOUND_ID + id));
+
+        productRepository.deleteById(id);
     }
 
-    @Override
-    public Product delete(Long id) {
-        return null;
+    private Product updateExistingProduct(Product existingProduct, Product newProductData) {
+        existingProduct.setName(newProductData.getName());
+        existingProduct.setDescription(newProductData.getDescription());
+        existingProduct.setMaterial(newProductData.getMaterial());
+        existingProduct.setColor(newProductData.getColor());
+        existingProduct.setDesigner(newProductData.getDesigner());
+        existingProduct.setPrice(newProductData.getPrice());
+        existingProduct.setImages(newProductData.getImages());
+        existingProduct.setCategories(newProductData.getCategories());
+        existingProduct.setSizes(newProductData.getSizes());
+
+        return productRepository.save(existingProduct);
     }
 }
