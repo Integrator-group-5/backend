8b6a9030cd27772597da51db9ad28afc114ac009
tineojf <92743762+tineodev@users.noreply.github.com>
2024-10-26 09:35:26 -0500

    Feat - Add controller & service to product by id

--- src/main/java/com/digitalhouse/backend/controller/ProductController.java
+++ src/main/java/com/digitalhouse/backend/controller/ProductController.java
@@ -2,10 +2,12 @@ package com.digitalhouse.backend.controller;
 
 
 import com.digitalhouse.backend.entity.ProductModel;
+import com.digitalhouse.backend.exception.ResourceNotFoundException;
 import com.digitalhouse.backend.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,6 +30,21 @@ public class ProductController {
                 cr = new CustomResponse(true, "Products found", productos);
             }
             return ResponseEntity.status(200).body(cr);
+        } catch (Exception e) {
+            CustomResponse cr = new CustomResponse(false, "Error in DB: " + e.getMessage(), null);
+            return ResponseEntity.status(500).body(cr);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CustomResponse> getProductById(@PathVariable Long id) {
+        try {
+            ProductModel product = productService.findById(id);
+            CustomResponse cr = new CustomResponse(true, "Product found", product);
+            return ResponseEntity.status(200).body(cr);
+        } catch (ResourceNotFoundException e) {
+            CustomResponse cr = new CustomResponse(false, "Product not found", e.getMessage());
+            return ResponseEntity.status(404).body(cr);
         } catch (Exception e) {
             CustomResponse cr = new CustomResponse(false, "Error in DB: " + e.getMessage(), null);
             return ResponseEntity.status(500).body(cr);
--- src/main/java/com/digitalhouse/backend/service/ProductService.java
+++ src/main/java/com/digitalhouse/backend/service/ProductService.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService implements IService<ProductModel> {
@@ -22,7 +23,12 @@ public class ProductService implements IService<ProductModel> {
 
     @Override
     public ProductModel findById(Long id) throws ResourceNotFoundException {
-        return null;
+        Optional<ProductModel> product = productRepository.findById(id);
+        if (product.isPresent()) {
+            return product.get();
+        } else {
+            throw new ResourceNotFoundException("Product not found", "id", id);
+        }
     }
 
     @Override
