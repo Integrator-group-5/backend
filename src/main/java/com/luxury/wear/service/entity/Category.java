dd4a55da1c1436906883fa758301dd8d5cda42da
DavidBlanco2825 <davidfernando2825@hotmail.com>
2024-10-25 19:42:34 -0500

    feat: define Category class with attributes and relationships
    - Created `Category` class to represent a category in the application.
    - Defined all necessary attributes and relationships to other entities for accurate data modeling.

---
+++ src/main/java/com/luxury/wear/service/entity/Category.java
@@ -0,0 +1,37 @@
+package com.luxury.wear.service.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "category")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @ManyToMany(mappedBy = "categories")
+    @JsonBackReference
+    private List<Product> products;
+}
