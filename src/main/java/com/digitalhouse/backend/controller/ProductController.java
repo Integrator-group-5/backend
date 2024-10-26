98f331333e84166d995f8402831233037388bf90
tineojf <92743762+tineodev@users.noreply.github.com>
2024-10-26 09:55:34 -0500

    Feat - Add cross origin *

--- src/main/java/com/digitalhouse/backend/controller/ProductController.java
+++ src/main/java/com/digitalhouse/backend/controller/ProductController.java
@@ -6,15 +6,13 @@ import com.digitalhouse.backend.exception.ResourceNotFoundException;
 import com.digitalhouse.backend.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/products")
+@CrossOrigin(origins = "*")
 public class ProductController {
     @Autowired
     private ProductService productService;
