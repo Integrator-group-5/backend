d1ad739331614964120fda22f4d27cc17ca5d8ca
tineojf <92743762+tineodev@users.noreply.github.com>
2024-11-04 12:44:21 -0500

    feat: create User entity implementing UserDetails & configure security config

--- src/main/java/com/luxury/wear/service/security/SecurityConfig.java
+++ src/main/java/com/luxury/wear/service/security/SecurityConfig.java
@@ -4,7 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -14,7 +15,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((authorizeRequests) -> authorizeRequests.anyRequest().permitAll());
-        //http.csrf(AbstractHttpConfigurer::disable); // Disable CSRF
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.headers((headers) -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
 
         return http.build();
     }
