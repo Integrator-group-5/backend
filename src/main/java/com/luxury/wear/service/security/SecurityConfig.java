f1928ffe1cc22066922b3f5bea623a7cab3ad177
DavidBlanco2825 <davidfernando2825@hotmail.com>
2024-11-05 17:49:40 -0500

    Implement role-based authentication and authorization in SecurityConfig
    - Configured security rules for specific endpoints based on roles:
      - `USER` or `ADMIN` roles required for certain user-related endpoints.
      - `ADMIN` role required for product management and user management endpoints.
      - Public access allowed for authentication and product browsing endpoints.
    - Integrated JwtAuthenticationFilter to handle JWT authentication.
    - Disabled CSRF protection for stateless API.

--- src/main/java/com/luxury/wear/service/security/SecurityConfig.java
+++ src/main/java/com/luxury/wear/service/security/SecurityConfig.java
@@ -1,23 +1,55 @@
 package com.luxury.wear.service.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((authorizeRequests) -> authorizeRequests.anyRequest().permitAll());
-        http.csrf(AbstractHttpConfigurer::disable);
-        http.headers((headers) -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((authorizeRequests) -> authorizeRequests
+                        // Endpoints accessible without authentication
+                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/api/v1/products", "/api/v1/products/page/{page}", "/api/v1/products/top-rents").permitAll()
+                        .requestMatchers("/api/v1/categories", "/api/v1/sizes").permitAll()
+
+                        // Endpoints requiring authentication with USER or ADMIN roles
+                        .requestMatchers("/api/v1/users/email", "/api/v1/users/{id}").hasAnyRole("USER", "ADMIN")
+
+                        // Endpoints requiring authentication with ADMIN role
+                        .requestMatchers("/api/v1/products").hasRole("ADMIN")
+                        .requestMatchers("/api/v1/products/delete-product/{id}").hasRole("ADMIN")
+                        .requestMatchers("/api/v1/users/delete-user/{id}").hasRole("ADMIN")
+                        .requestMatchers("/api/v1/users/page/{page}", "/api/v1/users", "/api/v1/users").hasRole("ADMIN")
+                        .requestMatchers("/api/v1/users").hasRole("ADMIN")
+
+                        // All other requests
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 }
+
