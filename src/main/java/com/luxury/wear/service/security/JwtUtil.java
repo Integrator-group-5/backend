14abbf90cb6676700a08d0a6da8abf6b46918a9f
DavidBlanco2825 <davidfernando2825@hotmail.com>
2024-11-05 16:55:25 -0500

    feat: add JwtUtil class for JWT operations and update application properties
    - Defined `JwtUtil` class to handle JWT actions such as token creation, validation, and parsing.
    - Added new environment variables (`jwt.secret`, `jwt.expiration.access-token`, `jwt.expiration.refresh-token`) in `application.properties` for JWT configuration.

---
+++ src/main/java/com/luxury/wear/service/security/JwtUtil.java
@@ -0,0 +1,71 @@
+package com.luxury.wear.service.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    @Value("${jwt.expiration.access-token}")
+    private long jwtExpirationAccessToken;
+
+    @Value("${jwt.expiration.refresh-token}")
+    private long jwtExpirationRefreshToken;
+
+    private final SecretKey secretKey;
+
+    public JwtUtil(@Value("${jwt.secret}") String jwtSecret) {
+        this.secretKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+
+    public String generateAccessToken(String email, String role) {
+        return Jwts.builder()
+                .subject(email)
+                .claim("role", role)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + jwtExpirationAccessToken))
+                .signWith(secretKey)
+                .compact();
+    }
+
+    public String generateRefreshToken(String email) {
+        return Jwts.builder()
+                .subject(email)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + jwtExpirationRefreshToken))
+                .signWith(secretKey)
+                .compact();
+    }
+
+    public String getEmailFromToken(String token) {
+        return parseClaims(token).getSubject();
+    }
+
+    private Claims parseClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser()
+                    .verifyWith(secretKey)
+                    .build()
+                    .parseSignedClaims(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+}
