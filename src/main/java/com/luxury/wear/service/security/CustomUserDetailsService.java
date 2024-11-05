d9eecd25c95f4649893014e0149724c68d6a1065
DavidBlanco2825 <davidfernando2825@hotmail.com>
2024-11-05 17:13:05 -0500

    feat: add CustomUserDetailsService and JwtAuthenticationFilter for JWT authentication
    - Defined `CustomUserDetailsService` to provide a custom implementation of `UserDetailsService`, enabling user retrieval for authentication.
    - Added `JwtAuthenticationFilter` to handle JWT authentication for incoming requests, ensuring secure access based on token validation.

---
+++ src/main/java/com/luxury/wear/service/security/CustomUserDetailsService.java
@@ -0,0 +1,20 @@
+package com.luxury.wear.service.security;
+
+import com.luxury.wear.service.service.user.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userService.findByEmail(username);
+    }
+}
