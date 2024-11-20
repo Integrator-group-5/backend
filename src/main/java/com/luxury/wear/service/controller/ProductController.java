4e534afb804c37756517eb95f6545f91af69cd26
DavidBlanco2825 <davidfernando2825@hotmail.com>
2024-11-19 19:54:37 -0500

    feat: enhance getAllProductsPaginated to support filtering by date range and search query
    - Updated the `getAllProductsPaginated` method in `ProductController` to accept new parameters:
      - Date range for filtering products by availability.
      - A search query for filtering products by name or description.
    - Enables more flexible and dynamic product retrieval based on user input.

--- src/main/java/com/luxury/wear/service/controller/ProductController.java
+++ src/main/java/com/luxury/wear/service/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.luxury.wear.service.controller;
 
 import com.luxury.wear.service.entity.Product;
 import com.luxury.wear.service.service.product.ProductService;
+import com.luxury.wear.service.service.reservation.ReservationService;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -10,7 +11,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/products")
@@ -18,6 +22,7 @@ import java.util.List;
 public class ProductController {
 
     private final ProductService productService;
+    private final ReservationService reservationService;
 
     @PostMapping
     public ResponseEntity<Product> createProduct(@RequestBody Product product) {
@@ -47,12 +52,22 @@ public class ProductController {
     public ResponseEntity<Page<Product>> getAllProductsPaginated(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "6") int size,
-            @RequestParam String category) {
+            @RequestParam(defaultValue = "") String category,
+            @RequestParam(required = false) LocalDate startDate,
+            @RequestParam(required = false) LocalDate endDate,
+            @RequestParam(defaultValue = "") String search) {
+
         Pageable pageable = PageRequest.of(page, size);
         Page<Product> products = productService.getAllProducts(pageable);
+
         if (category != null && !category.isEmpty()) {
             products = productService.getProductsByCategory(category, pageable);
         }
+
+        if (startDate != null && endDate != null && (search != null && !search.isEmpty())) {
+            products = productService.getAvailableProducts(startDate, endDate, search, pageable);
+        }
+
         return ResponseEntity.status(HttpStatus.OK).body(products);
     }
 
@@ -66,5 +81,15 @@ public class ProductController {
     public ResponseEntity<Void> deleteProduct(@PathVariable("id") Long id) {
         productService.deleteProductById(id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @GetMapping("/{id}/availability")
+    public ResponseEntity<Map<String, List<LocalDate>>> getProductAvailability(@PathVariable Long id) {
+
+        List<LocalDate> unavailableDates = reservationService.getUnavailableDates(id);
+        Map<String, List<LocalDate>> response = new HashMap<>();
+        response.put("unavailableDates", unavailableDates);
+
+        return ResponseEntity.ok(response);
     }
 }
