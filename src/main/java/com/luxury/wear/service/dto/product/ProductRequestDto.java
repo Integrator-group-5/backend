4a19d4d020b2da08983679133e385a07b91b1863
DavidBlanco2825 <davidfernando2825@hotmail.com>
2024-11-21 17:15:52 -0500

    feat: add ProductRequestDto and ProductResponseDto classes
    - Defined `ProductRequestDto` for handling incoming user data.
    - Defined `ProductResponseDto` for structuring outgoing user data.
    - These DTOs provide a clear separation between the Product entity and API data exchange.

---
+++ src/main/java/com/luxury/wear/service/dto/product/ProductRequestDto.java
@@ -0,0 +1,38 @@
+package com.luxury.wear.service.dto.product;
+
+import com.luxury.wear.service.entity.Category;
+import com.luxury.wear.service.entity.Image;
+import com.luxury.wear.service.entity.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductRequestDto {
+
+    private String name;
+
+    private String reference;
+
+    private String description;
+
+    private String material;
+
+    private String color;
+
+    private String designer;
+
+    private BigDecimal price;
+
+    private List<Image> images = new ArrayList<>();
+
+    private Category category;
+    
+    private List<Size> sizes = new ArrayList<>();
+}
