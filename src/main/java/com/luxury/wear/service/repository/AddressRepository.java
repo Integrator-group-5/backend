10ecdb196b3745a12d12d8774b3728904bb166a2
DavidBlanco2825 <davidfernando2825@hotmail.com>
2024-11-29 19:14:12 -0500

    feat: Define AddressRepository interface for Address entity operations
    - Created the `AddressRepository` interface to enable CRUD operations for `Address` entities.
    - This repository provides the foundation for managing address data in the application.

---
+++ src/main/java/com/luxury/wear/service/repository/AddressRepository.java
@@ -0,0 +1,7 @@
+package com.luxury.wear.service.repository;
+
+import com.luxury.wear.service.entity.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddressRepository extends JpaRepository<Address, Long> {
+}
